80550e24c87ab76fe6afd61532bf396437cb3277
WhoMan <enes515254@gmail.com>
2020-08-18 08:07:57 +0600

    Update TODO

--- maze.py
+++ maze.py
@@ -1,7 +1,8 @@
 import os
-import subprocess
-import sys
 import time
+
+
+# TODO: Сделать контроль над параметрами
 
 
 class Maze():
@@ -14,23 +15,15 @@ class Maze():
         self.w = value
 
     def set_height(self, value: int):
-        self.h = value
-    
-    def get_width(self):
-        return self.w
-    
-    def get_height(self):
-        return self.h
+        if value > 3:
+            self.h = value
 
     def build_maze(self): # Построить лабиринт
         # Запускаем файл, который строит лабиринт и возращает картинку
         # Переносим данный файл в папку испольняемой программы(Бота)
-        if sys.platform == "win32":
-            os.startfile(os.path.join(self.mb_path, "build/maze_builder"))
-        else:
-            dir = os.getcwd()
-            os.chdir(self.mb_path)
-            os.system("./build/maze_builder")
-            time.sleep(2)
-            os.chdir(dir)
-            os.rename(os.path.join(self.mb_path, "maze.bmp"), os.path.join(os.getcwd(), "maze.bmp"))
+        dir = os.getcwd()
+        os.chdir(self.mb_path)
+        os.system("./build/maze_builder -w " + str(self.w) + " -h " + str(self.h))
+        time.sleep(2)
+        os.chdir(dir)
+        os.rename(os.path.join(self.mb_path, "maze.bmp"), os.path.join(os.getcwd(), "maze.bmp"))
